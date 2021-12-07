0a53aa49e3eff841200144f64628a9893bd913ca
nafeal3mri <info@nafe.me>
2021-12-08 00:09:08 +0300

    dbfix

--- app/Http/Controllers/API/APImainController.php
+++ app/Http/Controllers/API/APImainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\DomainList;
 use Illuminate\Http\Request;
 use DB;
 class APImainController extends Controller
@@ -14,11 +15,19 @@ class APImainController extends Controller
         ],[
             'domain.required' => __('Please enter a valid url')
         ]);
-    
         $dataset = ['posted_domain' => $data['domain']];
         $pieces = parse_url($data['domain']);
         $domain = isset($pieces['host']) ? $pieces['host'] : '';
         if (preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs)) {
+            $domainres = DomainList::where(['domain' => $regs['domain']])->get();
+            $domain_color = '';
+            $domain_cat = $domainres->category;
+            $domain_desc = $domainres->description;
+            if($domainres->count() > 0){
+                $domain_color = $domainres->type;
+                $domain_cat = $domainres->category;
+                $domain_desc = $domainres->description;
+            }
             $resp = true;
             $dataset = [
                 'posted_domain' => $data['domain'],
