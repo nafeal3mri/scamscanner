33e31af5cedb9ea8ad4c6bba8ee4b9a2d41ed21b
nafeal3mri <info@nafe.me>
2022-04-23 21:42:19 +0300

    fix site meta

--- app/Models/sitemeta.php
+++ app/Models/sitemeta.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class sitemeta extends Model
+class Sitemeta extends Model
 {
     use HasFactory;
 }
