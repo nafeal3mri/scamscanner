d3db9ed6e72930c9ac849d85f3943f52758a9a22
nafeal3mri <info@nafe.me>
2022-08-21 01:27:14 +0300

    fix add list options

--- resources/views/vendor/backpack/crud/fields/depend_selector_parent.blade.php
+++ resources/views/vendor/backpack/crud/fields/depend_selector_parent.blade.php
@@ -4,7 +4,7 @@
         @foreach ($field['values'] as $key => $option)
             <option 
            {{ old($field['name']) && old($field['name']) == $key ? 'selected' : 
-           (isset($option[$field['value_val']]) && $option[$field['value_val']] == $key ? 'selected' :
+           (isset($field['value']) && $field['value'] == $option[$field['value_val']] ? 'selected' :
            (isset($field['default']) && $field['default'] == $key ? 'selected' : ''))}}
            value="{{$option[$field['value_val']]}}"
            data-onselect="{{$option[$field['depend_on_val']]}}"
